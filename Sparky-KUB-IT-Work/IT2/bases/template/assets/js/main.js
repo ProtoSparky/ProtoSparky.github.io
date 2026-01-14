41938e0c17ff8f077dff78a7a144c34ab6daa21b
Sparky <kristupmaker@gmail.com>
2026-01-14 09:44:13 +0100

    clearing out repo

--- Sparky-KUB-IT-Work/IT2/bases/template/assets/js/main.js
+++
@@ -1,3 +0,0 @@
-function init(){
-    console.info("init run");
-}
--- Sparky-KUB-IT-Work/IT2/bits/16.10.2023_Visma_but_bad/assets/js/init.js
+++
@@ -1,8 +0,0 @@
-function init(){
-    const TableLength = DataOP(1, true, "TableData").length;
-    for(let CurrentTablePointer = 0; CurrentTablePointer <TableLength; CurrentTablePointer++){
-        SpawnTable(DataOP(1, true, "TableData")[CurrentTablePointer],["TableArea","table0"+ CurrentTablePointer,"table",]);
-    }   
-   
-    
-}
